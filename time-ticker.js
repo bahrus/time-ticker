6ded210c0f5a4471fcbf6063381a99d623ab5105
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-02-06 18:39:46 -0500

    ...

--- time-ticker.js
+++ time-ticker.js
@@ -1,120 +1,91 @@
-import { xc } from 'xtal-element/lib/XtalCore.js';
+import { XE } from 'xtal-element/src/XE.js';
 import { animationInterval } from './animationInterval.js';
-const onIdxChange = ({ idx, items, self }) => {
-    if (idx === undefined)
-        return;
-    self.value = {
-        idx: idx,
-        item: (items && items.length > idx) ? items[idx] : null,
-        time: new Date(),
-    };
-    self.dispatchEvent(new CustomEvent('tick', {
-        detail: self.value
-    }));
-    if (self.wait) {
-        self.disabled = true;
-    }
-};
-const onEnabled = ({ enabled, self }) => {
-    self.disabled = !enabled;
-};
-const onItems = ({ items, self }) => {
-    if (items === undefined)
-        return;
-    self.repeat = items.length;
-};
-const onDuration = ({ duration, disabled, self }) => {
-    if (duration === undefined)
-        return;
-    if (disabled) {
-        if (self.controller !== undefined) {
-            self.controller.abort();
-            delete self.controller;
-            return;
-        }
-    }
-    if (self.controller)
-        return;
-    // https://youtu.be/MCi6AZMkxcU?t=719 nope
-    // https://youtu.be/MCi6AZMkxcU?t=918 nope
-    // https://youtu.be/MCi6AZMkxcU?t=1152 nope
-    // https://youtu.be/MCi6AZMkxcU?t=1169 nope
-    // https://youtu.be/MCi6AZMkxcU?t=1189 nope
-    // https://gist.github.com/jakearchibald/cb03f15670817001b1157e62a076fe95
-    self.controller = new AbortController();
-    // Create an animation callback every second:
-    animationInterval(duration, self.controller.signal, time => {
-        self.nextIdx();
-    });
-    self.nextIdx();
-};
-const propActions = [onIdxChange, onEnabled, onItems, onDuration];
-const obj = {
-    type: Object,
-    async: true
-};
-const bool = {
-    type: Boolean,
-    async: false,
-    reflect: true,
-};
-const num = {
-    type: Number,
-    async: true,
-    reflect: true,
-};
-const propDefMap = {
-    items: obj, value: obj,
-    idx: num, duration: num, repeat: num,
-    enabled: bool, disabled: bool, loop: bool, wait: bool,
-};
-/**
- * @element time-ticker
- * @event tick - Dispatched every time timer goes off
- */
 export class TimeTicker extends HTMLElement {
-    constructor() {
-        super(...arguments);
-        this.self = this;
-        this.propActions = propActions;
-        this.reactor = new xc.Rx(this);
-        /**
-         * Number of times to repeat before setting counter back to 0
-         * Setting Items will set this automatically
-         */
-        this.repeat = Infinity;
-    }
-    attributeChangedCallback(n, ov, nv) {
-        this.disabled = nv !== null;
-    }
-    connectedCallback() {
-        this.style.display = 'none';
-        xc.hydrate(this, slicedPropDefs, {
-            idx: -1,
-            duration: 1000,
-            repeat: Infinity,
-            enabled: true,
+    start({ duration }) {
+        const controller = new AbortController();
+        animationInterval(duration, controller.signal, time => {
+            this.ticks++;
         });
+        return {
+            controller,
+            ticks: this.ticks++,
+        };
     }
-    onPropChange(name, propDef, newVal) {
-        this.reactor.addToQueue(propDef, newVal);
+    onDisabled({}) {
+        return {
+            controller: undefined,
+        };
     }
-    nextIdx() {
-        let newIdx = this.idx;
-        if (newIdx >= this.repeat - 1) {
-            if (this.loop) {
-                newIdx = -1;
+    onItems({ items }) {
+        return {
+            repeat: items.length,
+        };
+    }
+    onTicks({ idx, repeat, loop, wait, items }) {
+        if (idx >= repeat - 1) {
+            if (loop) {
+                idx = -1;
             }
             else {
-                this.disabled = true;
-                return;
+                return {
+                    disabled: true,
+                };
             }
         }
-        this.idx = newIdx + 1;
+        idx++;
+        return {
+            idx,
+            disabled: wait,
+            value: {
+                idx,
+                item: (items && items.length > idx) ? items[idx] : undefined,
+            }
+        };
     }
 }
-TimeTicker.is = 'time-ticker';
-TimeTicker.observedAttributes = ['disabled'];
-const slicedPropDefs = xc.getSlicedPropDefs(propDefMap);
-xc.letThereBeProps(TimeTicker, slicedPropDefs.propDefs, 'onPropChange');
-xc.define(TimeTicker);
+const xe = new XE({
+    config: {
+        tagName: 'time-ticker',
+        propDefaults: {
+            wait: false,
+            ticks: 0,
+            idx: -1,
+            duration: 1_000,
+            repeat: Infinity,
+            enabled: true,
+            disabled: false,
+            loop: false,
+        },
+        propInfo: {
+            enabled: {
+                dry: false,
+                notify: {
+                    toggleTo: 'disabled',
+                }
+            },
+            value: {
+                notify: {
+                    dispatch: true,
+                },
+                parse: false,
+            },
+        },
+        style: {
+            display: 'none',
+        },
+        actions: {
+            onDisabled: 'disabled',
+            onItems: 'items',
+            start: {
+                ifAllOf: ['duration'],
+                ifNoneOf: ['disabled'],
+            },
+            onTicks: {
+                ifAllOf: ['ticks'],
+                ifKeyIn: ['repeat', 'loop', 'items'],
+                ifNoneOf: ['disabled'],
+            }
+        }
+    },
+    superclass: TimeTicker,
+});
--- time-ticker.ts
+++ time-ticker.ts
@@ -1,157 +1,100 @@
-import {xc, PropAction, PropDef, PropDefMap, ReactiveSurface} from 'xtal-element/lib/XtalCore.js';
-import {IValue} from './types.d.js';
+import {TimeTickerProps, TimeTickerActions} from './types';
+import {XE} from 'xtal-element/src/XE.js';
 import {animationInterval} from './animationInterval.js';
 
-const onIdxChange = ({idx, items, self}: TimeTicker) =>{
-    if(idx === undefined) return;
-    self.value = {
-        idx: idx,
-        item: (items && items.length > idx) ? items[idx] : null,
-        time: new Date(),
-    }
-    self.dispatchEvent(new CustomEvent('tick', {
-        detail: self.value
-    }));
-    if(self.wait){
-        self.disabled = true;
-    }
-}
-const onEnabled = ({enabled, self}: TimeTicker) => {
-    self.disabled = !enabled;
-}
-const onItems = ({items, self}: TimeTicker) => {
-    if(items === undefined) return;
-    self.repeat = items.length;
-}
-const onDuration = ({duration, disabled, self}: TimeTicker) => {
-    if(duration === undefined) return;
-    if(disabled){
-        if(self.controller !== undefined){
-            self.controller.abort();
-            delete self.controller;
-            return;
+export class TimeTicker extends HTMLElement implements TimeTickerActions{
+
+    start({duration}: this) {
+        const controller = new AbortController();
+        animationInterval(duration, controller.signal, time => {
+            this.ticks++;
+        });
+        return {
+            controller,
+            ticks: this.ticks++,
         }
     }
-    if(self.controller) return;
-    // https://youtu.be/MCi6AZMkxcU?t=719 nope
-    // https://youtu.be/MCi6AZMkxcU?t=918 nope
-    // https://youtu.be/MCi6AZMkxcU?t=1152 nope
-    // https://youtu.be/MCi6AZMkxcU?t=1169 nope
-    // https://youtu.be/MCi6AZMkxcU?t=1189 nope
-    // https://gist.github.com/jakearchibald/cb03f15670817001b1157e62a076fe95
-    self.controller = new AbortController();
 
-    // Create an animation callback every second:
-    animationInterval(duration, self.controller.signal, time => {
-        self.nextIdx();
-    });
-    self.nextIdx();
-}
-
-const propActions = [onIdxChange, onEnabled, onItems, onDuration] as PropAction[];
-const obj: PropDef = {
-    type: Object,
-    async: true
-}
-const bool: PropDef = {
-    type: Boolean,
-    async: false,
-    reflect: true,
-}
-const num: PropDef = {
-    type: Number,
-    async: true,
-    reflect: true,
-}
-const propDefMap: PropDefMap<TimeTicker> = {
-    items: obj, value:obj,
-    idx: num, duration: num, repeat: num,
-    enabled: bool, disabled: bool, loop: bool, wait: bool,
-}
-/**
- * @element time-ticker
- * @event tick - Dispatched every time timer goes off
- */
-export class TimeTicker extends HTMLElement implements ReactiveSurface{
-    static is = 'time-ticker';
-    static observedAttributes = ['disabled'];
-    self = this;
-    propActions = propActions;
-    reactor = new xc.Rx(this);
-    controller: AbortController | undefined;
-    /**
-     * Current count
-    */
-   idx: number | undefined;// = -1;
-   disabled: boolean | undefined;
-   value: IValue | undefined;
-    /**
-     * Remove disabled
-     */
-    enabled: boolean | undefined;
-    /**
-     * Items to rotate through.
-     * Sets property repeat to the number of items (length)
-     * Attribute support (must be in JSON format)
-     * @attr
-     */
-    items!: any[];
-
-    /**
-     * Number of milliseconds to wait
-     * @attr
-     */
-    duration: number | undefined;
-
-    /** 
-     * Number of times to repeat before setting counter back to 0
-     * Setting Items will set this automatically
-     */
-    repeat: number | undefined = Infinity;
-
-    /**
-     * Indicates whether should cycle or stop
-     * @attr
-     */
-    loop: boolean | undefined;
-
-    /**
-     * Disable after every tick
-     * @attr
-    */
-    wait: boolean | undefined;
-    attributeChangedCallback(n: string, ov: string, nv: string){
-        this.disabled = nv !== null;
+    onDisabled({}: this) {
+        return {
+            controller: undefined,
+        }
     }
-    connectedCallback(){
-        this.style.display = 'none';
-        xc.hydrate<Partial<TimeTicker>>(this, slicedPropDefs, {
+
+    onItems({items}: this){
+        return {
+            repeat: items.length,
+        }
+    }
+
+    onTicks({idx, repeat, loop, wait, items}: this){
+        if(idx >= repeat - 1){
+            if(loop){
+                idx = -1;
+            }else{
+                return {
+                    disabled: true,
+                }
+            }
+        }
+        idx++;
+        return {
+            idx,
+            disabled: wait,
+            value: {
+                idx,
+                item: (items && items.length > idx) ? items[idx] : undefined,
+            }
+        }
+    }
+}
+
+export interface TimeTicker extends TimeTickerProps{}
+
+const xe = new XE<TimeTickerProps, TimeTickerActions>({
+    config:{
+        tagName: 'time-ticker',
+        propDefaults: {
+            wait: false,
+            ticks: 0,
             idx: -1,
             duration: 1_000,
             repeat: Infinity,
             enabled: true,
-        });
-    }
-    
-    onPropChange(name: string, propDef: PropDef, newVal: any){
-        this.reactor.addToQueue(propDef, newVal);
-    }
-
-    nextIdx(){
-        let newIdx = this.idx!;
-        if(newIdx >= this.repeat! - 1){
-            if(this.loop){
-                newIdx = -1;
-            }else{
-                this.disabled = true;
-                return;
+            disabled: false,
+            loop: false,
+        },
+        propInfo:{
+            enabled:{
+                dry: false,
+                notify: {
+                    toggleTo: 'disabled',
+                }
+            },
+            value: {
+                notify: {
+                    dispatch: true,
+                },
+                parse: false,
+            },
+        },
+        style: {
+            display: 'none',
+        },
+        actions: {
+            onDisabled:'disabled',
+            onItems:'items',
+            start:{
+                ifAllOf: ['duration'],
+                ifNoneOf: ['disabled'],
+            },
+            onTicks: {
+                ifAllOf: ['ticks'],
+                ifKeyIn: ['repeat', 'loop', 'items'],
+                ifNoneOf: ['disabled'],
             }
         }
-        this.idx = newIdx + 1;
-    }
-}
+    },
+    superclass: TimeTicker,
+});
 
-
-const slicedPropDefs = xc.getSlicedPropDefs(propDefMap);
-xc.letThereBeProps(TimeTicker, slicedPropDefs.propDefs, 'onPropChange');
-xc.define(TimeTicker);
