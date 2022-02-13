66e34d50796e05dbf6cd97a9ed4281f577ffae26
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-02-13 14:08:21 -0500

    0.0.16

---
+++ doc.mjs
@@ -0,0 +1,4 @@
+import { CustomElementManifestGenerator } from 'may-it-be/doc.js';
+import { readFileSync } from 'fs';
+const schema = readFileSync('./schema.json', 'utf8');
+const cemg = new CustomElementManifestGenerator(schema, console.log);
